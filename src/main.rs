d33138d5adea0fa13df70b4fdac7a5ca2433f97e
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2024-07-07 00:53:59 +0530

    fix exit code

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use std::io::{stderr, Write};
 mod lexer;
 use lexer::lexer::Lexer;
 
+const EXIT_FILE_ERROR: i32 = 1;
+const EXIT_LEXICAL_ERROR: i32 = 65;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(stderr(), "Usage: {} <command> <filename>", args[0])
             .expect("Failed to write to stderr");
-        std::process::exit(1);
+        std::process::exit(EXIT_FILE_ERROR);
     }
 
     let command = &args[1];
@@ -26,11 +29,16 @@ fn main() {
     match command.as_str() {
         "tokenize" => {
             let (valid_tokens, invalid_tokens) = lexer.get_tokens();
-            for token in invalid_tokens {
+
+            invalid_tokens.iter().for_each(|token| {
                 println!("{}", token.tokenized_string());
-            }
-            for token in valid_tokens {
+            });
+            valid_tokens.iter().for_each(|token| {
                 println!("{}", token.tokenized_string());
+            });
+
+            if !invalid_tokens.is_empty() {
+                std::process::exit(EXIT_LEXICAL_ERROR);
             }
         }
 
