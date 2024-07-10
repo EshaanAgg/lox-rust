bde40f3a7eafb899dfcc3655c016a2e3f71aadc9
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2024-07-10 11:02:38 +0530

    regenerate rust code with the gen script

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -4,27 +4,30 @@ use crate::lexer::types::TokenType;
 pub enum Expr {
     Unary(Token, Box<Expr>),
     Binary(Box<Expr>, Token, Box<Expr>),
-    Literal(Token),
     Grouping(Box<Expr>),
+    Literal(Token),
 }
 
 pub trait Visitor<R> {
     fn visit_unary_expr(&self, op: &Token, expr: &Box<Expr>) -> R;
-    fn visit_binary_expr(&self, expr1: &Box<Expr>, op: &Token, expr2: &Box<Expr>) -> R;
-    fn visit_literal_expr(&self, token: &Token) -> R;
+    fn visit_binary_expr(&self, left: &Box<Expr>, op: &Token, right: &Box<Expr>) -> R;
     fn visit_grouping_expr(&self, expr: &Box<Expr>) -> R;
+    fn visit_literal_expr(&self, value: &Token) -> R;
 }
 
 impl Expr {
     pub fn accept<R>(&self, visitor: &impl Visitor<R>) -> R {
         match self {
             Expr::Unary(op, expr) => visitor.visit_unary_expr(op, expr),
-            Expr::Binary(expr1, op, expr2) => visitor.visit_binary_expr(expr1, op, expr2),
-            Expr::Literal(token) => visitor.visit_literal_expr(token),
+            Expr::Binary(left, op, right) => visitor.visit_binary_expr(left, op, right),
             Expr::Grouping(expr) => visitor.visit_grouping_expr(expr),
+            Expr::Literal(value) => visitor.visit_literal_expr(value),
         }
     }
+}
 
+// Custom implementations for the Expr enum.
+impl Expr {
     /// Creates a new unary expression with the given operator and expression.
     pub fn new_string_literal(value: &str) -> Expr {
         Expr::Literal(Token::new_default(
